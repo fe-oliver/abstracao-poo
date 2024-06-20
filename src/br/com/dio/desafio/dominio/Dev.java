2db72fac164a413fb9b557c12d2edd69fe0b1246
fe-oliver <nandacelia.dias@gmail.com>
2024-06-20 16:02:51 -0300

    20/06

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,6 +1,9 @@
 package br.com.dio.desafio.dominio;
 
-import java.util.*;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 
 public class Dev {
     private String nome;
@@ -23,19 +26,17 @@ public class Dev {
     }
 
     public double calcularTotalXp() {
-        Iterator<Conteudo> iterator = this.conteudosConcluidos.iterator();
-        double soma = 0;
-        while(iterator.hasNext()){
-            double next = iterator.next().calcularXp();
-            soma += next;
-        }
-        return soma;
+        return this.conteudosConcluidos
+            .stream()
+            .mapToDouble(Conteudo::calcularXp)
+            .sum();
+    }
 
         /*return this.conteudosConcluidos
                 .stream()
                 .mapToDouble(Conteudo::calcularXp)
                 .sum();*/
-    }
+    
 
 
     public String getNome() {
